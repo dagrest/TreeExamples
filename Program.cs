ff23e8e1025a3fa540b9c10ca5315c3674e023c3
David Agrest <david.agrest@snyk.io>
2022-07-30 10:50:56 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TreeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tree Examples - VS Code");
+        }
+    }
+}
